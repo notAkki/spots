63dd8f543969b479d18522fb0d5b85906781a632
Akshar Barot <akshar.barot67@gmail.com>
2024-11-04 16:51:34 -0500

    Added information pop-over

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,16 +5,12 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import Map from "@/components/Map";
 import Loading from "@/components/Loading";
 import Image from "next/image";
-import { Alert, AlertDescription } from "@/components/ui/alert";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
 
 interface dataFormat {
     building: string;
@@ -119,15 +115,135 @@ export default function Home() {
                         height={200}
                         alt="Logo"
                     />
+                    <Popover>
+                        <PopoverTrigger className="">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width={28}
+                                height={28}
+                                fill={"none"}
+                            >
+                                <path
+                                    d="M22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12Z"
+                                    stroke="currentColor"
+                                    strokeWidth="1.7"
+                                />
+                                <path
+                                    d="M12.2422 17V12C12.2422 11.5286 12.2422 11.2929 12.0957 11.1464C11.9493 11 11.7136 11 11.2422 11"
+                                    stroke="currentColor"
+                                    strokeWidth="1.7"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                                <path
+                                    d="M11.992 8H12.001"
+                                    stroke="currentColor"
+                                    strokeWidth="2.2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+                        </PopoverTrigger>
+                        <PopoverContent className="bg-zinc-900 border-zinc-600 text-zinc-200 w-72">
+                            <div className="font-bold mb-1">
+                                Important Notes:
+                            </div>
+                            <ul className="list-disc pl-4">
+                                <li>
+                                    Building/room access may be restricted to
+                                    specific colleges or departments
+                                </li>
+                                <li>
+                                    Displayed availability only reflects
+                                    official class schedules
+                                </li>
+                                <li>
+                                    Rooms may be occupied by unofficial meetings
+                                    or study groups
+                                </li>
+                                <li>
+                                    Click on indicators to view room schedules
+                                    for that building
+                                </li>
+                            </ul>
+                            {/* <div className="text-sm mt-2 pl-4">
+                                Built by{" "}
+                                <a href="https://www.aksharbarot.com/">
+                                    Akshar Barot
+                                </a>
+                            </div> */}
+                        </PopoverContent>
+                    </Popover>
                 </div>
                 <ScrollArea className="h-full">
-                    <div className="w-full h-20 pl-8 pr-4 flex sm:hidden justify-between items-center">
+                    <div className="w-full h-20 pl-8 pr-8 flex sm:hidden justify-between items-center">
                         <Image
                             src={"/logo.png"}
                             width={180}
                             height={200}
                             alt="Logo"
                         />
+                        <Popover>
+                            <PopoverTrigger className="">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    width={28}
+                                    height={28}
+                                    fill={"none"}
+                                >
+                                    <path
+                                        d="M22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12Z"
+                                        stroke="currentColor"
+                                        strokeWidth="1.7"
+                                    />
+                                    <path
+                                        d="M12.2422 17V12C12.2422 11.5286 12.2422 11.2929 12.0957 11.1464C11.9493 11 11.7136 11 11.2422 11"
+                                        stroke="currentColor"
+                                        strokeWidth="1.7"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                    <path
+                                        d="M11.992 8H12.001"
+                                        stroke="currentColor"
+                                        strokeWidth="2.2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                </svg>
+                            </PopoverTrigger>
+                            <PopoverContent className="bg-zinc-900 border-zinc-600 text-zinc-200 w-72">
+                                <div className="font-bold mb-1">
+                                    Important Notes:
+                                </div>
+                                <ul className="list-disc pl-4">
+                                    <li>
+                                        Building/room access may be restricted
+                                        to specific colleges or departments
+                                    </li>
+                                    <li>
+                                        Displayed availability only reflects
+                                        official class schedules
+                                    </li>
+                                    <li>
+                                        Rooms may be occupied by unofficial
+                                        meetings or study groups
+                                    </li>
+                                    <li>
+                                        Click on indicators to view room
+                                        schedules for that building
+                                    </li>
+                                </ul>
+                                {/* <div className="text-sm mt-2 pl-4">
+                                Built by{" "}
+                                <a href="https://www.aksharbarot.com/">
+                                    Akshar Barot
+                                </a>
+                            </div> */}
+                            </PopoverContent>
+                        </Popover>
                     </div>
                     <Left
                         data={data}
